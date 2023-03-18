87562efbde05aa7394794c0793739af5695d59c1
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-03-18 22:12:48 +0530

    second commit

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-console.log("Hello World");
-console.clear()
 
+let tablinks = document.getElementsByClassName("tab-links")
+let tabcontents = document.getElementsByClassName("tab-contents")
 
-document.querySelector('.cross').style.display = 'none';
-
-document.querySelector('.hamBurger').addEventListener("click", () => {
-document.querySelector('.sidebar').classList.toggle('sidebarGo');
-    if (document.querySelector('.sidebar').classList.contains('sidebarGo')) {
-        document.querySelector('.ham').style.display = 'inline'
-        document.querySelector('.cross').style.display = 'none'
-    }
-    else {
-        document.querySelector('.ham').style.display = 'none'
-        setTimeout(() => {
-            document.querySelector('.cross').style.display = 'inline'
-        }, 300)
-
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
     }
 
-});
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+
+
+
+let sideMenu = document.getElementById("sideMenu");
+
+function openmenu() {
+    sideMenu.style.right = "0";
+}
+
+function closemenu() {
+    sideMenu.style.right = "-200px";
+}
+
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxk4C_HfvgigYpfKb_Ftq1i1UFRDWQpiKTcLHLL_utwRT184j7d5_wBB0QO65U9CowlaA/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully."
+            setTimeout(() => {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
 
